de366c9c74e3645cee81535a7e78e9f3eac1ee52
Mark N. Beebe <mbeebe08@gmail.com>
2020-09-29 23:27:08 -0700

    Add data binding functionality to MainActivity

--- app/src/main/java/com/example/android/aboutme/MyName.kt
+++ app/src/main/java/com/example/android/aboutme/MyName.kt
@@ -16,4 +16,8 @@
 
 package com.example.android.aboutme
 
+import android.provider.ContactsContract
+
+data class MyName(var name: String = "", var nickname: String = "")
+
 // Create data class MyName for the name and nickname here.
